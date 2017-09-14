1cc2dc8be6531269304ca6b5c8c0bed3df510118
marko delgadillo <markoneseven@gmail.com>
2017-09-14 10:52:52 -0700

    Made the changes from the review of the previous pull request.

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ var $userBtn = document.querySelectorAll(".menu-btns")
 
 function hide(event) {
   $users.classList.toggle("hidden")
-  var $id = event.target.getAttribute("id")
+  var id = event.target.getAttribute("id")
     for (var i = 0; i < $userBtn.length; i++) {
-      if ($id === "teacher"){
+      if (id === "teacher"){
           $teacher.classList.toggle("hidden")
-      } else if ($id === "student") {
+      } else if (id === "student") {
           $student.classList.toggle("hidden")
         } else {
           $parent.classList.toggle("hidden")
