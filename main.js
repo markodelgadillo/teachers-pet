12b23c671f32fb052fed0dc47541e61823dca342
marko delgadillo <markoneseven@gmail.com>
2017-09-15 12:49:14 -0700

    Recreated the menu to select students with javascript and added functionality. #3

--- main.js
+++ main.js
@@ -1,8 +1,49 @@
+var students = [
+  {id:"1", name: "Melissa", grade:"5", parents: [{name1: "null", name2:"null"}]},
+  {id:"2", name: "Jeff", grade: "4", parents: [{name1:"null", name2:"null"}]},
+  {id:"3", name: "Vanessa", grade: "5", parents: [{name1:"null", name2:"null"}]}
+]
+
+var $div = document.createElement('div')
+var $menu = document.getElementById('student-menu')
+$menu.textContent = "Students"
+
+/* changes here too */
+for (var i = 0; i < students.length; i++) {
+  var $li = document.createElement('li')
+  $li.setAttribute('class', 'student-btns')
+  $li.textContent = students[i].name
+  $li.setAttribute('data-id', students[i].id) //here!
+  $menu.appendChild($li)
+}
+
+/* start here */
+function addName(event) {
+  var id = event.target.getAttribute('data-id')
+  for (var i = 0; i < students.length; i++) {
+    if (students[i].id === id) {
+       var $studentDetails = document.createElement('div')
+       $studentDetails.textContent = students[i].name
+       var $details = document.querySelector('.student-details')
+       $details.innerHTML = ''
+       $details.appendChild($studentDetails)
+    }
+  }
+}
+
+var $li = document.querySelectorAll('.student-btns')
+for (var i = 0; i < students.length; i++) {
+$li[i].addEventListener('click', addName)
+}
+
+
+
+
 var $users = document.querySelector("#container-users")
 var $teacher = document.querySelector("#teacher-container")
 var $student = document.querySelector("#student-container")
 var $parent = document.querySelector("#parent-container")
-var $userBtn = document.querySelectorAll(".menu-btns")
+var $userBtn = document.querySelectorAll(".menu1-btns")
 
 function hide(event) {
   $users.classList.toggle("hidden")
